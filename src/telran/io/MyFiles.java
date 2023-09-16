7aba4be426d375fdea4f50767b9ec1fc7e72f3f6
AlGuron <blizgur8@gmail.com>
2023-09-17 01:14:54 +0300

    init

---
+++ src/telran/io/MyFiles.java
@@ -0,0 +1,68 @@
+package telran.io;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import static java.nio.file.FileVisitResult.*;
+
+public class MyFiles implements FileVisitor<Object> {
+	public static void displayDir(String path, int maxDepth) throws IOException {
+		// TODO
+		// Throwing IllegalArgumentException in the case the given is not a directory
+		// Printing out the directory content using appropriate offsets showing which
+		// node belong to which directory
+		// printing type of node, for example
+		// input-output-network - dir
+		// src - dir
+		// telran - dir
+		// io - dir
+		// test - dir
+		// FileSystemTests.java - file
+		int offset = Path.of(path).toAbsolutePath().normalize().getNameCount();
+		try {
+			Files.walk(Path.of(path).toAbsolutePath().normalize(), maxDepth).forEach(p -> {
+				int depth = p.getNameCount();
+				System.out.println("   ".repeat(depth - offset) + p.getFileName() + " - " + getType(p));
+			});
+		} catch (IOException e) {
+
+		}
+		System.out.println("============================================");
+	}
+
+	private static String getType(Path p) {
+		return p.getFileName().toString().contains(".") ? "file" : "dir";
+
+	}
+
+	@Override
+	public FileVisitResult preVisitDirectory(Object dir, BasicFileAttributes attrs) throws IOException {
+		return CONTINUE;
+	}
+
+	@Override
+	public FileVisitResult visitFile(Object file, BasicFileAttributes attrs) throws IOException {
+		Path p = (Path) file;		
+		int depth = p.getNameCount();
+		
+		if(attrs.isRegularFile()) {
+			System.out.println("   ".repeat(depth ) + p.getFileName() + " - File" );
+		}		
+		
+		return CONTINUE;
+	}
+
+	@Override
+	public FileVisitResult visitFileFailed(Object file, IOException exc) throws IOException {
+		System.err.println(exc);
+		return CONTINUE;
+	}
+
+	@Override
+	public FileVisitResult postVisitDirectory(Object dir, IOException exc) throws IOException {
+		Path p = (Path) dir;
+		int depth = p.getNameCount();
+		System.out.println("   ".repeat(depth ) + p.getFileName() + " - Dir");
+		return CONTINUE;
+	}
+}
---
+++ src/telran/io/test/FileSystemTests.java
@@ -0,0 +1,59 @@
+package telran.io.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static java.nio.file.FileVisitOption.*;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.EnumSet;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import telran.io.MyFiles;
+
+class FileSystemTests {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	@Disabled
+	void test() throws IOException {
+		Path current = Path.of(".");
+		System.out.printf("current.getFileName() -> %s\n", current.getFileName());
+		assertTrue(Files.isDirectory(current));
+		assertFalse(current.isAbsolute());
+		current = current.toAbsolutePath();
+		assertTrue(current.isAbsolute());
+		System.out.printf("current.getFileName() -> %s\n", current.getFileName());
+		current = current.normalize();
+		
+
+		System.out.printf("current.toRealPath() -> %s\n", current.toRealPath());
+		System.out.printf("current.getNameCount() -> %s\n", current.getNameCount());
+		System.out.printf("current.getName(5) -> %s\n", current.getName(5));
+		System.out.printf("current.getFileName() -> %s\n", current.getFileName());
+		 Files.list(current).map(p -> p.getFileName()).forEach(p ->
+		 System.out.println(p));
+		 Files.walk(current).map(p -> p.getFileName()).forEach(p ->
+		 System.out.println(p));
+		Files.walk(current, 2).map(p -> p.getFileName()).forEach(p -> System.out.println(p));
+	}
+
+	@Test
+	void displayDirTest() throws IOException {
+		MyFiles.displayDir(".", 3);
+	}
+
+	@Test
+	void FileVisitor() throws IOException {
+		Path p = Path.of(".").normalize();
+		EnumSet<FileVisitOption> opts = EnumSet.of(FOLLOW_LINKS);
+		MyFiles fv = new MyFiles();
+		Files.walkFileTree(p, opts, 3, fv);
+	}
+
+}

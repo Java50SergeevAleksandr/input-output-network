1414d51035ec20c09afa151554cfe7a0f3c91091
AlGuron <blizgur8@gmail.com>
2023-09-17 20:46:59 +0300

    init

---
+++ src/telran/io/test/CopyAppl.java
@@ -0,0 +1,15 @@
+package telran.io.test;
+
+public class CopyAppl {
+
+	public static void main(String[] args) {
+		// args[0] - source file
+		// args[1] - destination file
+		// args[2] - "overwrite"
+		// TODO write application for copying from source file to destination file
+		// Implementation Requirement: to use while cycle with read call
+		// main must not contain throws declaration
+
+	}
+
+}
